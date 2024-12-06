566a337f19e6a0824b8e3cb798216e48bdd40302
cacaca <cacacaca@gmail.com>
2024-12-06 10:41:08 -0300

    teste de escopo

--- variaveis.js
+++ variaveis.js
@@ -1,15 +1,19 @@
+var nome="carol";
+console.log(nome);
+
 console.log("---- var ----");
 if (true) {
-    var exemploVar = "Visível fora do bloco!";
+    let nome="carolina";
+    console.log("Acessível dentro do bloco:" + nome);
 }
-console.log(exemploVar); // Funciona, mas pode causar problemas.
+console.log("Acessível fora do bloco:" + nome);                                                                                                       
 
 console.log("---- let ----");
 if (true) {
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-// console.log(exemploLet); // Erro: exemploLet não está definido.
+//console.log(exemploLet); // Erro: exemploLet não está definido.
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
